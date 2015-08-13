3547a73f085d4389adcda44d0df49121ed0c5bfa
brixmavu <brixton@projectcodex.co>
2015-08-13 16:16:51 +0200

    intro handlebars

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
    var express = require('express');
+   var exphbs = require('express-handlebars');
+
    var app = express();
+
+   app.engine('handlebars', exphbs({
+       defaultLayout: 'main'
+   }));
+   app.set('view engine', 'handlebars');
+
 
    app.use(express.static('public'));
 
-   // create a route
-   app.get('/', function (req, res) {
-     res.send('Hello World!');
+    // create a route
+   app.get('/', function(req, res) {
+       res.render('home');
    });
 
-   //start the server
-   var server = app.listen(3000, function () {
+    //start the server
+   var server = app.listen(3000, function() {
 
-     var host = server.address().address;
-     var port = server.address().port;
+       var host = server.address().address;
+       var port = server.address().port;
 
-     console.log('Example app listening at http://%s:%s', host, port);
+       console.log('Example app listening at http://%s:%s', host, port);
 
    });
-
