e3f6e7190feee2de71b27816cb4cc0f42c928f2d
brixmavu <brixton@projectcodex.co>
2015-09-15 15:26:54 +0200

    heroku deploy

--- server.js
+++ server.js
@@ -66,7 +66,7 @@
    });
 
    //start the server
-   var server = app.listen(3000, function() {
+   var server = app.listen(4000, function() {
 
        var host = server.address().address;
        var port = server.address().port;
