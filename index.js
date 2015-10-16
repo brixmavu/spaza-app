bb8f2e3ddf5ae461860598f5713234bcc00f7145
brixmavu <brixton@projectcodex.co>
2015-10-16 16:26:44 +0200

    changes 16/10/2015

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express'),
-    exphbs  = require('express-handlebars'),
+    exphbs = require('express-handlebars'),
     mysql = require('mysql'),
     myConnection = require('express-myconnection'),
     bodyParser = require('body-parser'),
@@ -10,15 +10,17 @@ var express = require('express'),
 var app = express();
 
 var dbOptions = {
-      host: 'localhost',
-      user: 'root',
-      password: 'brix2015',
-      port: 3306,
-      database: 'spaza_app'
+    host: 'localhost',
+    user: 'root',
+    password: 'brix2015',
+    port: 3306,
+    database: 'spaza_app'
 };
 
 //setup template handlebars as the template engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
@@ -26,13 +28,17 @@ app.use(express.static(__dirname + '/public'));
 //setup middleware
 app.use(myConnection(mysql, dbOptions, 'single'));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
+app.use(bodyParser.urlencoded({
+        extended: false
+    }))
+    // parse application/json
 app.use(bodyParser.json())
 
 function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.render('error', { error: err });
+    res.status(500);
+    res.render('error', {
+        error: err
+    });
 }
 
 //setup the handlers
@@ -51,6 +57,6 @@ app.use(errorHandler);
 var portNumber = process.env.CRUD_PORT_NR || 3001;
 
 //start everything up
-app.listen(portNumber, function () {
+app.listen(portNumber, function() {
     console.log('Create, Read, Update, and Delete (CRUD) example server listening on:', portNumber);
 });
--- routes/products.js
+++ routes/products.js
@@ -1,72 +1,78 @@
-
 /***
  * A very basic CRUD example using MySQL
  */
 
-exports.show = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err) return next(err);
-		connection.query('SELECT * from products', [], function(err, results) {
-		connection.query('SELECT * from categories', [], function(err, categories) { 
+exports.show = function(req, res, next) {
+    req.getConnection(function(err, connection) {
         if (err) return next(err);
-    		res.render( 'home', {
-					no_products : results.length === 0,
-					products : results,
-					categories : categories
-    		});
-    	});
-      });
-	});
+        connection.query('SELECT * from products', [], function(err, results) {
+            if (err) return next(err);
+            res.render('home', {
+                no_products: results.length === 0,
+                products: results,
+            });
+
+        });
+    });
 };
 
-exports.showAdd = function(req, res){
-	res.render('add');
+exports.showAdd = function(req, res) {
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * from categories', [], function(err, categories) {
+            res.render('add', {
+                categories: categories
+            });
+        });
+    });
 }
 
-exports.add = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err) return next(err);
-		var input = JSON.parse(JSON.stringify(req.body));
-		var data = {
-      		products_name : input.products_name,
-      		category_id : input.category_id
-  	};
-		connection.query('insert into products set ?', data, function(err, results, categories) {
-  		if (err) return next(err);
-			res.redirect('/products');
-		});
-	});
+exports.add = function(req, res, next) {
+    req.getConnection(function(err, connection) {
+        if (err) return next(err);
+        var input = JSON.parse(JSON.stringify(req.body));
+        var data = {
+            products_name: input.products_name,
+            category_id: input.category_id
+        };
+        connection.query('insert into products set ?', data, function(err, results) {
+            if (err) return next(err);
+            res.redirect('/products');
+        });
+    });
 };
 
-exports.get = function(req, res, next){
-	var products_id = req.params.products_id;
-	req.getConnection(function(err, connection){
-		connection.query('SELECT * FROM products WHERE products_id = ?', [products_id], function(err,rows){
-			if(err) return next(err);
-			res.render('edit',{page_title:"Edit Customers - Node.js", data : rows[0]});
-		});
-	});
+exports.get = function(req, res, next) {
+    var products_id = req.params.products_id;
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM products WHERE products_id = ?', [products_id], function(err, rows) {
+            if (err) return next(err);
+            res.render('edit', {
+                page_title: "Edit Customers - Node.js",
+                data: rows[0]
+            });
+        });
+    });
 };
 
-exports.update = function(req, res, next){
+exports.update = function(req, res, next) {
 
-	var data = JSON.parse(JSON.stringify(req.body));
-  var products_id = req.params.products_id;
-  req.getConnection(function(err, connection){
-			connection.query('UPDATE products SET ? WHERE products_id = ?', [data, products_id], function(err, rows){
-    			if (err) next(err);
-          res.redirect('/products');
-    		});
+    var data = JSON.parse(JSON.stringify(req.body));
+    var products_id = req.params.products_id;
+    req.getConnection(function(err, connection) {
+        connection.query('UPDATE products SET ? WHERE products_id = ?', [data, products_id], function(err, rows) {
+            if (err) next(err);
+            res.redirect('/products');
+        });
 
     });
 };
 
-exports.delete = function(req, res, next){
-	var products_id = req.params.products_id;
-	req.getConnection(function(err, connection){
-		connection.query('DELETE FROM products WHERE products_id = ?', [products_id], function(err,rows){
-			if(err) return next(err);
-			res.redirect('/products');
-		});
-	});
+exports.delete = function(req, res, next) {
+    var products_id = req.params.products_id;
+    req.getConnection(function(err, connection) {
+        connection.query('DELETE FROM products WHERE products_id = ?', [products_id], function(err, rows) {
+            if (err) return next(err);
+            res.redirect('/products');
+        });
+    });
 };
