8a6bb70ae7dbf40b769ce02e5b613d2f7a37da1e
brixmavu <brixton@projectcodex.co>
2016-04-08 14:53:36 +0200

    making search work

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,17 @@ exports.search = function (req, res, next) {
     if (err) {
       return next(err);
     };
+    var searchItem = req.param.searchItem;
+    searchItem = "%" + searchItem + "%";
 
-  })
-
-}
+    connection.query("select products_id, products_name from products where products_name like ?",[searchItem], function (err, results) {
+       if(err){
+         return next(err);
+       };
+       res.render("search", {
+         products: results,
+         user: req.session.user
+       });
+    });
+  });
+};
